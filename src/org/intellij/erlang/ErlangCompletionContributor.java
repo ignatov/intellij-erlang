e76ef940d00a69df677707f598a051eba52edf4b
ignatov <ignatovs@gmail.com>
2012-06-29 02:36:15 +0400

    keyword completion added

---
+++ src/org/intellij/erlang/ErlangCompletionContributor.java
@@ -0,0 +1,58 @@
+package org.intellij.erlang;
+
+import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiFileFactory;
+import com.intellij.psi.impl.source.tree.TreeUtil;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.util.ProcessingContext;
+import org.intellij.erlang.parser.ErlangLexer;
+import org.intellij.erlang.parser.GeneratedParserUtilBase;
+import org.intellij.erlang.psi.ErlangFile;
+import org.intellij.erlang.psi.impl.ErlangFileImpl;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+
+import static com.intellij.patterns.PlatformPatterns.psiElement;
+import static com.intellij.patterns.StandardPatterns.instanceOf;
+
+/**
+ * @author ignatov
+ */
+public class ErlangCompletionContributor extends CompletionContributor {
+  public ErlangCompletionContributor() {
+    extend(CompletionType.BASIC, psiElement().inFile(instanceOf(ErlangFileImpl.class)), new CompletionProvider<CompletionParameters>() {
+      @Override
+      protected void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet result) {
+        for (String keywords : suggestKeywords(parameters.getPosition())) {
+          result.addElement(LookupElementBuilder.create(keywords).setBold());
+        }
+      }
+    });
+  }
+
+  private static Collection<String> suggestKeywords(PsiElement position) {
+    TextRange posRange = position.getTextRange();
+    ErlangFile posFile = (ErlangFile) position.getContainingFile();
+//    final TextRange range = new TextRange(posRange.getStartOffset(), posRange.getStartOffset());
+    final TextRange range = new TextRange(0, posRange.getStartOffset());
+    final String text = range.isEmpty() ? CompletionInitializationContext.DUMMY_IDENTIFIER : range.substring(posFile.getText());
+
+    PsiFile file = PsiFileFactory.getInstance(posFile.getProject()).createFileFromText("a.erl", ErlangLanguage.INSTANCE, text, true, false);
+    int completionOffset = posRange.getStartOffset() - range.getStartOffset();
+    GeneratedParserUtilBase.CompletionState state = new GeneratedParserUtilBase.CompletionState(completionOffset) {
+      @Override
+      public String convertItem(Object o) {
+        if (o instanceof IElementType && ErlangLexer.KEYWORDS.contains((IElementType) o)) return o.toString();
+        return o instanceof String ? (String) o : null;
+      }
+    };
+    file.putUserData(GeneratedParserUtilBase.COMPLETION_STATE_KEY, state);
+    TreeUtil.ensureParsed(file.getNode());
+    return state.items;
+  }
+}
--- src/org/intellij/erlang/parser/ErlangLexer.java
+++ src/org/intellij/erlang/parser/ErlangLexer.java
@@ -3,13 +3,18 @@ package org.intellij.erlang.parser;
 import com.intellij.lexer.FlexAdapter;
 import com.intellij.lexer.LookAheadLexer;
 import com.intellij.lexer.MergingLexerAdapter;
+import com.intellij.psi.tree.TokenSet;
 
 import static org.intellij.erlang.ErlangParserDefinition.COMMENTS;
+import static org.intellij.erlang.ErlangTypes.*;
 
 /**
  * @author ignatov
  */
 public class ErlangLexer extends LookAheadLexer {
+  public static final TokenSet KEYWORDS = TokenSet.create(
+    ERL_AFTER, ERL_WHEN, ERL_BEGIN, ERL_END, ERL_OF, ERL_CASE, ERL_FUN, ERL_QUERY, ERL_CATCH, ERL_IF, ERL_RECEIVE, ERL_TRY);
+
   public ErlangLexer() {
     super(new MergingLexerAdapter(new FlexAdapter(new _ErlangLexer()), COMMENTS));
   }
