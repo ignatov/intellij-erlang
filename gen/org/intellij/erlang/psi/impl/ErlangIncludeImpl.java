5eea10bc9c08f5f117a0e31574e5aa3adf76596d
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2012-07-23 14:21:17 +0400

    forgotten ErlangInclude added

---
+++ gen/org/intellij/erlang/psi/impl/ErlangIncludeImpl.java
@@ -0,0 +1,30 @@
+// This is a generated file. Not intended for manual editing.
+package org.intellij.erlang.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static org.intellij.erlang.ErlangTypes.*;
+import org.intellij.erlang.psi.*;
+
+public class ErlangIncludeImpl extends ErlangCompositeElementImpl implements ErlangInclude {
+
+  public ErlangIncludeImpl(ASTNode node) {
+    super(node);
+  }
+
+  @Override
+  @Nullable
+  public PsiElement getString() {
+    return findChildByType(ERL_STRING);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof ErlangVisitor) ((ErlangVisitor)visitor).visitInclude(this);
+    else super.accept(visitor);
+  }
+
+}
