c542477940c4211ced4598bb9fd2fbf97b509e3a
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2012-08-02 17:16:37 +0400

    fixed #19

--- tests/org/intellij/erlang/parser/ErlangParserTest.java
+++ tests/org/intellij/erlang/parser/ErlangParserTest.java
@@ -37,29 +37,32 @@ public class ErlangParserTest extends ParsingTestCase {
     return true;
   }
 
-  public void testHelloWorld()        { doTest(true); }
-  public void testExport()            { doTest(true); }
-  public void testH()                 { doTest(true); }
-  public void testMnesia()            { doTest(true); }
-  public void testIsDigits()          { doTest(true); }
-  public void testDialyzerDataflow()  { doTest(true); }
-  public void testTest()              { doTest(true); }
-  public void testRecords()           { doTest(true); }
-  public void testDialyzerClParse()   { doTest(true); }
-  public void testMp4Mux()            { doTest(true); }
-  public void testComments()          { doTest(true); }
-  public void testMultiline()         { doTest(true); }
-  public void testRecordExpression()  { doTest(true); }
+  public void testHelloWorld()        { doTest(true, false); }
+  public void testExport()            { doTest(true, false); }
+  public void testH()                 { doTest(true, false); }
+  public void testMnesia()            { doTest(true, false); }
+  public void testIsDigits()          { doTest(true, false); }
+  public void testDialyzerDataflow()  { doTest(true, false); }
+  public void testTest()              { doTest(true, false); }
+  public void testRecords()           { doTest(true, false); }
+  public void testDialyzerClParse()   { doTest(true, false); }
+  public void testMp4Mux()            { doTest(true, false); }
+  public void testComments()          { doTest(true, false); }
+  public void testMultiline()         { doTest(true, false); }
+  public void testRecordExpression()  { doTest(true, false); }
+  public void testPeriods()           { doTest(true, true); }
+  public void test19()                { doTest(true, true); }
 
-  @Override
-  protected void doTest(boolean checkResult) {
+  protected void doTest(boolean checkResult, boolean suppressErrors) {
 //    OVERWRITE_TESTDATA = true;
 
     super.doTest(checkResult);
-    assertFalse(
-      "PsiFile contains error elements",
-      toParseTreeText(myFile, skipSpaces(), includeRanges()).contains("PsiErrorElement")
-    );
+    if (!suppressErrors) {
+      assertFalse(
+        "PsiFile contains error elements",
+        toParseTreeText(myFile, skipSpaces(), includeRanges()).contains("PsiErrorElement")
+      );
+    }
   }
 }
 
