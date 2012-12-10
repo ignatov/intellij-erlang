5cb905a48577830136b11609ab70bdc5dd3a7117
ignatov <ignatovs@gmail.com>
2012-12-11 00:31:51 +0400

    compilation fix

---
+++ gen/org/intellij/erlang/psi/impl/ErlangFunctionWithArityVariablesImpl.java
@@ -0,0 +1,36 @@
+// This is a generated file. Not intended for manual editing.
+package org.intellij.erlang.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static org.intellij.erlang.ErlangTypes.*;
+import org.intellij.erlang.psi.*;
+
+public class ErlangFunctionWithArityVariablesImpl extends ErlangCompositeElementImpl implements ErlangFunctionWithArityVariables {
+
+  public ErlangFunctionWithArityVariablesImpl(ASTNode node) {
+    super(node);
+  }
+
+  @Override
+  @NotNull
+  public List<ErlangQVar> getQVarList() {
+    return PsiTreeUtil.getChildrenOfTypeAsList(this, ErlangQVar.class);
+  }
+
+  @Override
+  @Nullable
+  public PsiElement getInteger() {
+    return findChildByType(ERL_INTEGER);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof ErlangVisitor) ((ErlangVisitor)visitor).visitFunctionWithArityVariables(this);
+    else super.accept(visitor);
+  }
+
+}
