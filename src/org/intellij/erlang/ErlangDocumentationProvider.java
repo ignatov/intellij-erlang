bb2ae67b1873c2d895ff4166404817f1f9438576
Maxim Vladimirsky <horkhe@gmail.com>
2012-12-21 00:29:57 +0400

    Introduce external documentation provider

--- src/org/intellij/erlang/ErlangDocumentationProvider.java
+++ src/org/intellij/erlang/ErlangDocumentationProvider.java
@@ -16,10 +16,18 @@
 
 package org.intellij.erlang;
 
+import com.intellij.codeInsight.documentation.PlatformDocumentationUtil;
 import com.intellij.lang.documentation.AbstractDocumentationProvider;
+import com.intellij.lang.documentation.CompositeDocumentationProvider;
+import com.intellij.lang.documentation.ExternalDocumentationProvider;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.*;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.Function;
 import com.intellij.util.containers.ContainerUtil;
@@ -30,34 +38,115 @@ import org.intellij.erlang.psi.ErlangSpecification;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Set;
 
 /**
  * @author ignatov
  */
-public class ErlangDocumentationProvider extends AbstractDocumentationProvider {
+public class ErlangDocumentationProvider extends AbstractDocumentationProvider implements ExternalDocumentationProvider {
+  @Nullable
   @Override
-  public String generateDoc(PsiElement element, PsiElement originalElement) {
-    if (element instanceof ErlangFunction) {
-      ErlangFunction prevFunction = PsiTreeUtil.getPrevSiblingOfType(element, ErlangFunction.class);
-      PsiComment comment = PsiTreeUtil.getPrevSiblingOfType(element, PsiComment.class);
-      ErlangAttribute attribute = PsiTreeUtil.getPrevSiblingOfType(element, ErlangAttribute.class);
-      PsiElement spec = attribute != null ? PsiTreeUtil.getChildOfType(attribute, ErlangSpecification.class) : null;
-      String commentText = "";
-      if (spec instanceof ErlangSpecification && notFromPreviousFunction(spec, prevFunction)) {
-        commentText += spec.getText().replaceFirst("spec", "<b>Specification:</b><br/>") + "<br/><br/>";
-      }
-      if (comment != null && comment.getTokenType() == ErlangParserDefinition.ERL_FUNCTION_DOC_COMMENT && notFromPreviousFunction(comment, prevFunction)) {
-        commentText += "<b>Comment:</b><br/>" + getCommentText(comment, "%%", EDOC_FUNCTION_TAGS);
-        return commentText;
+  public List<String> getUrlFor(@NotNull PsiElement element, @Nullable PsiElement originalElement) {
+    if (element instanceof ErlangModule) {
+      return findUrlForModule((ErlangModule) element);
+    }
+    else if (element instanceof ErlangFunction) {
+      return findUrlForFunction((ErlangFunction) element);
+    }
+    return null;
+  }
+
+  @Nullable
+  private static List<String> findUrlForModule(@NotNull ErlangModule erlModule) {
+    final VirtualFile virtualFile = getVirtualFile(erlModule);
+    if (virtualFile == null) {
+      return null;
+    }
+    return findUrlForVirtualFile(erlModule.getProject(), virtualFile, "");
+  }
+
+  @Nullable
+  private static List<String> findUrlForFunction(@NotNull ErlangFunction erlFunction) {
+    final VirtualFile virtualFile = getVirtualFile(erlFunction);
+    if (virtualFile == null) {
+      return null;
+    }
+    final String inDocRef = "#" + erlFunction.getName() + "-" + erlFunction.getArity();
+    return findUrlForVirtualFile(erlFunction.getProject(), virtualFile, inDocRef);
+  }
+
+  @Nullable
+  private static VirtualFile getVirtualFile(@NotNull PsiElement psiElement) {
+    final PsiFile containingFile = psiElement.getContainingFile();
+    return (containingFile == null ? null : containingFile.getVirtualFile());
+  }
+
+  @Nullable
+  private static List<String> findUrlForVirtualFile(@NotNull Project project,
+                                                    @NotNull VirtualFile virtualFile,
+                                                    @NotNull String inDocRef) {
+    final ProjectFileIndex fileIndex = ProjectRootManager.getInstance(project).getFileIndex();
+    final List<OrderEntry> orderEntries = fileIndex.getOrderEntriesForFile(virtualFile);
+    for (OrderEntry orderEntry : orderEntries) {
+      final String[] docRootUrls = JavadocOrderRootType.getUrls(orderEntry);
+      final String sdkHttpDocRelPath = sdkHttpDocRelPath(virtualFile);
+      final List<String> docUrls = PlatformDocumentationUtil.getHttpRoots(docRootUrls, sdkHttpDocRelPath + inDocRef);
+      if (docUrls != null) {
+        return docUrls;
       }
     }
+    return null;
+  }
+
+  @Nullable
+  private static String sdkHttpDocRelPath(@NotNull VirtualFile virtualFile) {
+    final String appDirName = virtualFile.getParent().getParent().getName();
+    final String prefix;
+    if (appDirName.startsWith("erts")) {
+      prefix = "";
+    }
+    else {
+      prefix = "lib/";
+    }
+    return prefix + appDirName + "/doc/html/" + virtualFile.getNameWithoutExtension() + ".html";
+  }
+
+  @Nullable
+  @Override
+  public String generateDoc(@NotNull PsiElement element, @Nullable PsiElement originalElement) {
+    if (element instanceof ErlangFunction) {
+      return generateFunctionDoc((ErlangFunction) element);
+    }
     else if (element instanceof ErlangModule) {
-      PsiElement parent = element.getParent();
-      PsiComment comment = PsiTreeUtil.getPrevSiblingOfType(parent, PsiComment.class);
-      if (comment != null && comment.getTokenType() == ErlangParserDefinition.ERL_MODULE_DOC_COMMENT) {
-        return getCommentText(comment, "%%%", EDOC_MODULE_TAGS);
-      }
+      return generateModuleDoc((ErlangModule) element);
+    }
+    return null;
+  }
+
+  @Nullable
+  private static String generateFunctionDoc(@NotNull ErlangFunction erlangFunction) {
+    ErlangFunction prevFunction = PsiTreeUtil.getPrevSiblingOfType(erlangFunction, ErlangFunction.class);
+    PsiComment comment = PsiTreeUtil.getPrevSiblingOfType(erlangFunction, PsiComment.class);
+    ErlangAttribute attribute = PsiTreeUtil.getPrevSiblingOfType(erlangFunction, ErlangAttribute.class);
+    PsiElement spec = attribute != null ? PsiTreeUtil.getChildOfType(attribute, ErlangSpecification.class) : null;
+    String commentText = "";
+    if (spec instanceof ErlangSpecification && notFromPreviousFunction(spec, prevFunction)) {
+      commentText += spec.getText().replaceFirst("spec", "<b>Specification:</b><br/>") + "<br/><br/>";
+    }
+    if (comment != null && comment.getTokenType() == ErlangParserDefinition.ERL_FUNCTION_DOC_COMMENT && notFromPreviousFunction(comment, prevFunction)) {
+      commentText += "<b>Comment:</b><br/>" + getCommentText(comment, "%%", EDOC_FUNCTION_TAGS);
+      return commentText;
+    }
+    return null;
+  }
+
+  @Nullable
+  private static String generateModuleDoc(@NotNull ErlangModule erlangModule) {
+    PsiElement parent = erlangModule.getParent();
+    PsiComment comment = PsiTreeUtil.getPrevSiblingOfType(parent, PsiComment.class);
+    if (comment != null && comment.getTokenType() == ErlangParserDefinition.ERL_MODULE_DOC_COMMENT) {
+      return getCommentText(comment, "%%%", EDOC_MODULE_TAGS);
     }
     return null;
   }
@@ -104,5 +193,25 @@ public class ErlangDocumentationProvider extends AbstractDocumentationProvider {
     EDOC_OVERVIEW_TAGS.addAll(EDOC_GENERIC_TAGS);
     EDOC_MODULE_TAGS.addAll(EDOC_GENERIC_TAGS);
     EDOC_FUNCTION_TAGS.addAll(EDOC_GENERIC_TAGS);
+  }
+
+  @Nullable
+  @Override
+  public String fetchExternalDocumentation(@NotNull Project project, @NotNull PsiElement element, @NotNull List<String> docUrls) {
+    return null;  // TODO Implement
+  }
+
+  @Override
+  public boolean hasDocumentationFor(@NotNull PsiElement element, @Nullable PsiElement originalElement) {
+    return CompositeDocumentationProvider.hasUrlsFor(this, element, originalElement);
+  }
+
+  @Override
+  public boolean canPromptToConfigureDocumentation(@NotNull PsiElement element) {
+    return false;
+  }
+
+  @Override
+  public void promptToConfigureDocumentation(@NotNull PsiElement element) {
   }
 }
