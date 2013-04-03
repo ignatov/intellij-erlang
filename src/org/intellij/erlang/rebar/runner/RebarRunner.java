0c4ae31826c81b09bdd45498b1f9b8728263af94
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2013-04-03 20:17:07 +0400

    fix in rebar runner

--- src/org/intellij/erlang/rebar/runner/RebarRunner.java
+++ src/org/intellij/erlang/rebar/runner/RebarRunner.java
@@ -14,22 +14,6 @@
  * limitations under the License.
  */
 
-/*
- * Copyright 2012 Sergey Ignatov
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.intellij.erlang.rebar.runner;
 
 import com.intellij.execution.ExecutionException;
@@ -45,6 +29,7 @@ import com.intellij.execution.ui.RunContentDescriptor;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public final class RebarRunner extends DefaultProgramRunner {
   public static final String REBAR_RUNNER_ID = "RebarRunner";
@@ -61,11 +46,11 @@ public final class RebarRunner extends DefaultProgramRunner {
   }
 
   @Override
-  protected RunContentDescriptor doExecute(@NotNull Project project,
-                                           @NotNull Executor executor,
-                                           @NotNull RunProfileState state,
-                                           @NotNull RunContentDescriptor contentToReuse,
-                                           @NotNull ExecutionEnvironment env)
+  protected RunContentDescriptor doExecute(Project project,
+                                           Executor executor,
+                                           RunProfileState state,
+                                           RunContentDescriptor contentToReuse,
+                                           ExecutionEnvironment env)
     throws ExecutionException {
     FileDocumentManager.getInstance().saveAllDocuments();
     ExecutionResult result = state.execute(executor, this);
