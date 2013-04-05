96b0c98fe5c4ab87989e1cf27a94c930ade1ef72
Sergey Ignatov <ignatovs@gmail.com>
2013-04-06 00:55:11 +0400

    resolve fix, test data

--- testData/highlighting/VariableResolve.erl
+++ testData/highlighting/VariableResolve.erl
@@ -1,7 +1,4 @@
--author("ignatov").
-
-%% API
--export([name_to_pid/1]).
+-export([get_profile/2, name_to_pid/1]).
 
 name_to_pid(Name) ->
   case Name of
@@ -9,7 +6,26 @@ name_to_pid(Name) ->
       case Name of
         undefined ->
           exit(could_not_find_registered_name);
-        <warning>Pid</warning> -> null
+            <warning>Pid</warning> -> null;
+        <warning>Pid</warning> -> 1
       end;
-    <warning>Pid</warning> -> 1
+    _ -> 1
+  end.
+
+warning(_What, _List) -> ok.
+
+get_profile(APIKey, DeviceID) when is_binary(APIKey),
+  is_binary(DeviceID) ->
+  case ets:lookup(cache_workers, APIKey) of
+    [] ->
+      warning("Failed to lookup cache worker pid. APIKey=~p", [APIKey]),
+      notfound;
+    [{_, _, _, Pid}] ->
+      case is_process_alive(Pid) of
+        true ->
+          gen_server:<warning>call</warning>(Pid, {get_profile, DeviceID}); % todo: mock it
+        false ->
+          lager:error("Cache worker is no longer alive! APIKey=~p", [APIKey]),
+          invalid_pid
+      end
   end.
