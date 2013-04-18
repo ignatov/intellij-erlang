cf904ab20ba65c56d595849ec6ebad3a8a5c6130
Sergey Ignatov <ignatovs@gmail.com>
2013-04-19 00:56:14 +0400

    fixed #113

---
+++ src/org/intellij/erlang/ErlangUpdateAddedFileProcessor.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2012-2013 Sergey Ignatov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.intellij.erlang;
+
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.impl.file.UpdateAddedFileProcessor;
+import com.intellij.util.IncorrectOperationException;
+import org.intellij.erlang.psi.ErlangFile;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author ignatov
+ */
+public class ErlangUpdateAddedFileProcessor extends UpdateAddedFileProcessor {
+  @Override
+  public boolean canProcessElement(PsiFile file) {
+    return file instanceof ErlangFile && file.getFileType() == ErlangFileType.MODULE;
+  }
+
+  @Override
+  public void update(PsiFile element, @Nullable PsiFile originalElement) throws IncorrectOperationException {
+    element.setName(element.getName());
+  }
+}
