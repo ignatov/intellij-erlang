0529105ecc971856a8c044a7c10e16caabfa08ad
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2013-06-05 17:29:38 +0400

    comment based injection

---
+++ src/org/intellij/erlang/editor/ExtendedPsiCommentManipulator.java
@@ -0,0 +1,19 @@
+package org.intellij.erlang.editor;
+
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiComment;
+import com.intellij.psi.impl.source.resolve.reference.impl.manipulators.PsiCommentManipulator;
+
+/**
+ * @author ignatov
+ */
+public class ExtendedPsiCommentManipulator extends PsiCommentManipulator {
+  @Override
+  public TextRange getRangeInElement(PsiComment element) {
+    final String text = element.getText();
+    if (text.startsWith("%%%")) return new TextRange(3, element.getTextLength());
+    if (text.startsWith("%%")) return new TextRange(2, element.getTextLength());
+    if (text.startsWith("%")) return new TextRange(1, element.getTextLength());
+    return super.getRangeInElement(element);
+  }
+}
---
+++ src/org/intellij/erlang/psi/impl/ErlangASTFactory.java
@@ -0,0 +1,22 @@
+package org.intellij.erlang.psi.impl;
+
+import com.intellij.lang.DefaultASTFactoryImpl;
+import com.intellij.psi.impl.source.tree.LeafElement;
+import com.intellij.psi.impl.source.tree.PsiCommentImpl;
+import com.intellij.psi.tree.IElementType;
+
+/**
+ * @author ignatov
+ */
+public class ErlangASTFactory extends DefaultASTFactoryImpl {
+  @Override
+  public LeafElement createComment(IElementType type, CharSequence text) {
+    return new ErlangCommentImpl(type, text);
+  }
+
+  public static class ErlangCommentImpl extends PsiCommentImpl {
+    public ErlangCommentImpl(IElementType type, CharSequence text) {
+      super(type, text);
+    }
+  }
+}
