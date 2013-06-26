5217341181f4f363d7f18bf520afae32f2f6095f
Sergey Savenko <deadok22@gmail.com>
2013-06-26 14:09:25 +0400

    added tests for IntroduceRecord quickfix

---
+++ testData/quickfixes/introduce_record/simple.erl
@@ -0,0 +1,1 @@
+foo() -> #test_recor<caret>d
---
+++ tests/org/intellij/erlang/quickfixes/ErlangIntroduceRecordFixTest.java
@@ -0,0 +1,29 @@
+package org.intellij.erlang.quickfixes;
+
+import org.intellij.erlang.inspection.ErlangUnresolvedRecordInspection;
+
+/**
+ * @author savenko
+ */
+public class ErlangIntroduceRecordFixTest extends ErlangQuickFixTestBase {
+
+  @SuppressWarnings("unchecked")
+  @Override
+  protected void setUp() throws Exception {
+    System.setProperty("idea.platform.prefix", "Idea");
+    super.setUp();
+    myFixture.enableInspections(ErlangUnresolvedRecordInspection.class);
+  }
+
+  @Override
+  protected String getTestDataPath() {
+    return "testData/quickfixes/introduce_record/";
+  }
+
+  private void doIntroduceRecordTest()                        { doTest("Introduce new record"); }
+
+  public void testSimple() throws Throwable                   { doIntroduceRecordTest(); }
+  public void testWithPrecedingFunctions() throws Throwable   { doIntroduceRecordTest(); }
+  public void testWithPrecedingRecords() throws Throwable     { doIntroduceRecordTest(); }
+  public void testSurroundedByDeclarations() throws Throwable { doIntroduceRecordTest(); }
+}
