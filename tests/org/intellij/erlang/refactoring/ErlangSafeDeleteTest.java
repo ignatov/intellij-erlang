c7e040cd97c31dca05bebf99b3002daa88454084
Sergey Ignatov <ignatovs@gmail.com>
2013-11-01 02:31:16 +0400

    fixed #359: remove export on delete function

---
+++ tests/org/intellij/erlang/refactoring/ErlangSafeDeleteTest.java
@@ -0,0 +1,32 @@
+package org.intellij.erlang.refactoring;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.refactoring.safeDelete.SafeDeleteHandler;
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+
+/**
+ * @author ignatov
+ */
+public class ErlangSafeDeleteTest extends LightCodeInsightFixtureTestCase {
+  @Override
+  protected void setUp() throws Exception {
+    System.setProperty("idea.platform.prefix", "Idea");
+    super.setUp();
+  }
+
+  @Override
+    protected String getTestDataPath() {
+      return "testData/refactoring/delete/";
+    }
+
+  private void doTest() {
+    final String testName = getTestName(true);
+    myFixture.configureByFile(testName + ".erl");
+    PsiElement element = myFixture.getElementAtCaret();
+    SafeDeleteHandler.invoke(myFixture.getProject(), new PsiElement[]{element}, false);
+    myFixture.checkResultByFile(testName + "-after.erl");
+  }
+
+  public void testFunction()          throws Exception { doTest(); }
+  public void testWithExports()       throws Exception { doTest(); }
+}
