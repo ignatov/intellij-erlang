cfcf42d9f4d102f84221fec930e5e6fffceca4f1
Sergey Savenko <deadok22@gmail.com>
2013-11-30 15:14:46 +0400

    add macro substituting lexer tests to main test case

--- tests/org/intellij/erlang/ErlangTestCase.java
+++ tests/org/intellij/erlang/ErlangTestCase.java
@@ -36,6 +36,7 @@ import org.intellij.erlang.highlighting.generate.ErlangGenerateTest;
 import org.intellij.erlang.info.ErlangParameterInfoHandlerTest;
 import org.intellij.erlang.marker.ErlangMethodSeparatorProviderTest;
 import org.intellij.erlang.lexer.ErlangFormsLexerTest;
+import org.intellij.erlang.lexer.ErlangMacroSubstitutingLexerTest;
 import org.intellij.erlang.navigation.ErlangGotoSuperTest;
 import org.intellij.erlang.parser.ErlangAppParserTest;
 import org.intellij.erlang.parser.ErlangParserTest;
@@ -61,6 +62,7 @@ public class ErlangTestCase extends TestCase {
     TestSuite suite = new TestSuite();
     suite.addTestSuite(ErlangParserTest.class);
     suite.addTestSuite(ErlangFormsLexerTest.class);
+    suite.addTestSuite(ErlangMacroSubstitutingLexerTest.class);
     suite.addTestSuite(ErlangAppParserTest.class);
     suite.addTestSuite(ErlangHighlightingTest.class);
     suite.addTestSuite(Erlang17HighlightingTest.class);
