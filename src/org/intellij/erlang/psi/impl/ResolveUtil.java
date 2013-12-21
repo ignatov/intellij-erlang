ca337a0be77c1bfcf2b9901a6981befad2975e95
Sergey Ignatov <ignatovs@gmail.com>
2013-12-21 14:26:57 +0400

    ResolveUtil cleanup

--- src/org/intellij/erlang/psi/impl/ResolveUtil.java
+++ src/org/intellij/erlang/psi/impl/ResolveUtil.java
@@ -17,45 +17,38 @@
 package org.intellij.erlang.psi.impl;
 
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiNamedElement;
 import com.intellij.psi.ResolveState;
-import com.intellij.psi.scope.NameHint;
 import com.intellij.psi.scope.PsiScopeProcessor;
 import com.intellij.psi.util.PsiTreeUtil;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public abstract class ResolveUtil {
-  public static boolean treeWalkUp(@Nullable PsiElement place, PsiScopeProcessor processor) {
+  private ResolveUtil() {
+  }
+
+  public static boolean treeWalkUp(@Nullable PsiElement place, @NotNull PsiScopeProcessor processor) {
     PsiElement lastParent = null;
     PsiElement run = place;
     while (run != null) {
       if (place != run && !run.processDeclarations(processor, ResolveState.initial(), lastParent, place)) return false;
       lastParent = run;
-      run = run.getContext(); //same as getParent
+      run = run.getParent();
     }
-
     return true;
   }
 
-  public static boolean processChildren(PsiElement element, PsiScopeProcessor processor,
-                                        ResolveState substitutor, PsiElement lastParent, PsiElement place) {
+  public static boolean processChildren(@NotNull PsiElement element,
+                                        @NotNull PsiScopeProcessor processor,
+                                        @NotNull ResolveState substitutor,
+                                        @Nullable PsiElement lastParent,
+                                        @NotNull PsiElement place) {
     PsiElement run = lastParent == null ? element.getLastChild() : lastParent.getPrevSibling();
     while (run != null) {
       if (PsiTreeUtil.findCommonParent(place, run) != run && !run.processDeclarations(processor, substitutor, null, place)) {
         return false;
       }
       run = run.getPrevSibling();
-    }
-
-    return true;
-  }
-
-  public static boolean processElement(PsiScopeProcessor processor, PsiNamedElement namedElement) {
-    if (namedElement == null) return true;
-    NameHint nameHint = processor.getHint(NameHint.KEY);
-    String name = nameHint == null ? null : nameHint.getName(ResolveState.initial());
-    if (name == null || name.equals(namedElement.getName())) {
-      return processor.execute(namedElement, ResolveState.initial());
     }
     return true;
   }
