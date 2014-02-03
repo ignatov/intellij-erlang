b9f6de4963db24faba83ed31e08b8997505f5c4a
Sergey Savenko <deadok22@gmail.com>
2014-02-03 15:29:30 +0400

    add mark as include root action

---
+++ src/org/intellij/erlang/roots/ErlangMarkIncludeSourceRootAction.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2012-2014 Sergey Ignatov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.intellij.erlang.roots;
+
+import com.intellij.ide.projectView.actions.MarkSourceRootAction;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleType;
+import org.intellij.erlang.ErlangModuleType;
+import org.intellij.erlang.jps.model.ErlangIncludeSourceRootType;
+import org.jetbrains.annotations.NotNull;
+
+@SuppressWarnings("ComponentNotRegistered")
+public class ErlangMarkIncludeSourceRootAction extends MarkSourceRootAction {
+  public ErlangMarkIncludeSourceRootAction() {
+    super(ErlangIncludeSourceRootType.INSTANCE);
+  }
+
+  @Override
+  protected boolean isEnabled(@NotNull RootsSelection selection, @NotNull Module module) {
+    return super.isEnabled(selection, module) && ModuleType.get(module) == ErlangModuleType.getInstance();
+  }
+}
