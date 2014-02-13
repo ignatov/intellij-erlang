55e8634b34c8535e347a01c9645fa63cc5885892
Sergey Savenko <deadok22@gmail.com>
2014-02-13 16:57:32 +0400

    approximate executable lines for line breakpoint setting

--- src/org/intellij/erlang/debugger/xdebug/ErlangLineBreakpointType.java
+++ src/org/intellij/erlang/debugger/xdebug/ErlangLineBreakpointType.java
@@ -1,9 +1,18 @@
 package org.intellij.erlang.debugger.xdebug;
 
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.Processor;
+import com.intellij.xdebugger.XDebuggerUtil;
 import com.intellij.xdebugger.breakpoints.XLineBreakpointType;
 import org.intellij.erlang.ErlangFileType;
+import org.intellij.erlang.ErlangTypes;
+import org.intellij.erlang.psi.*;
+import org.intellij.erlang.psi.impl.ErlangPsiImplUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -23,7 +32,50 @@ public class ErlangLineBreakpointType extends XLineBreakpointType<ErlangLineBrea
 
   @Override
   public boolean canPutAt(@NotNull VirtualFile file, int line, @NotNull Project project) {
-    // todo: check if line breakpoint can be set here (see "Executable Lines" at http://www.erlang.org/doc/apps/debugger/debugger_chapter.html)
-    return file.getFileType() == ErlangFileType.MODULE;
+    if (file.getFileType() != ErlangFileType.MODULE) return false;
+    return isLineBreakpointAvailable(file, line, project);
+  }
+
+  // it should return true for lines matching "Executable Lines"
+  // description at http://www.erlang.org/doc/apps/debugger/debugger_chapter.html
+  // and, ideally, it should return false otherwise
+  private static boolean isLineBreakpointAvailable(VirtualFile file, int line, @NotNull Project project) {
+    Document document = FileDocumentManager.getInstance().getDocument(file);
+    if (document == null) return false;
+    LineBreakpointAvailabilityProcessor canPutAtChecker = new LineBreakpointAvailabilityProcessor();
+    XDebuggerUtil.getInstance().iterateLine(project, document, line, canPutAtChecker);
+    return canPutAtChecker.isLineBreakpointAvailable();
+  }
+
+  private static final class LineBreakpointAvailabilityProcessor implements Processor<PsiElement> {
+    private boolean myIsLineBreakpointAvailable;
+
+    @Override
+    public boolean process(PsiElement psiElement) {
+      if (ErlangPsiImplUtil.isWhitespaceOrComment(psiElement) ||
+        psiElement.getNode().getElementType() == ErlangTypes.ERL_DOT ||
+        psiElement.getNode().getElementType() == ErlangTypes.ERL_ARROW) return true;
+      @SuppressWarnings("unchecked")
+      ErlangCompositeElement nonExecutableParent = PsiTreeUtil.getParentOfType(psiElement,
+          ErlangGuard.class,
+          ErlangArgumentDefinition.class,
+          ErlangAttribute.class,
+          ErlangRecordDefinition.class,
+          ErlangIncludeLib.class,
+          ErlangInclude.class,
+          ErlangMacrosDefinition.class,
+          ErlangTypeDefinition.class);
+      if (nonExecutableParent != null) return true;
+      ErlangClauseBody executableParent = PsiTreeUtil.getParentOfType(psiElement, ErlangClauseBody.class);
+      if (executableParent != null) {
+        myIsLineBreakpointAvailable = true;
+        return false;
+      }
+      return true;
+    }
+
+    public boolean isLineBreakpointAvailable() {
+      return myIsLineBreakpointAvailable;
+    }
   }
 }
