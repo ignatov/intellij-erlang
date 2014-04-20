62083b3a174319474982b3de87dde08d0e358053
Sergey Ignatov <ignatovs@gmail.com>
2014-04-20 15:16:08 +0400

    revert icon

--- src/org/intellij/erlang/ErlangCompletionContributor.java
+++ src/org/intellij/erlang/ErlangCompletionContributor.java
@@ -462,7 +462,7 @@ public class ErlangCompletionContributor extends CompletionContributor {
       result.addElement(
         PrioritizedLookupElement.withPriority(
           LookupElementBuilder.create(name)
-            .withIcon(ErlangIcons.FILE)
+            .withIcon(ErlangIcons.MODULE)
             .withInsertHandler(new ModuleInsertHandler(project, name, withColon)),
           MODULE_PRIORITY));
     }
