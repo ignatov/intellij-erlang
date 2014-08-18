113d22582f6e337e7d3ea1a76a3134a3959027c6
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2014-08-18 14:06:19 +0400

    copyright updated

--- src/org/intellij/erlang/formatter/settings/ErlangCodeStyleSettings.java
+++ src/org/intellij/erlang/formatter/settings/ErlangCodeStyleSettings.java
@@ -1,5 +1,5 @@
 /*
-* Copyright 2012 Volodymyr Kyrychenko <vladimir.kirichenko@gmail.com>
+ * Copyright 2012-2014 Sergey Ignatov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
