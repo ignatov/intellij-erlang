fceeedd2d84170bea49c2467ec397bdab96983c3
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2014-09-15 17:19:31 +0400

    extract console icon

--- src/org/intellij/erlang/icons/ErlangIcons.java
+++ src/org/intellij/erlang/icons/ErlangIcons.java
@@ -55,7 +55,8 @@ public interface ErlangIcons {
   Icon REBAR = IconLoader.getIcon("/icons/rebar-16.png");
   Icon REBAR_MODULE_CONFLICT = AllIcons.Actions.Cancel;
   Icon REBAR_EUNIT = new LayeredIcon(REBAR, AllIcons.Nodes.JunitTestMark);
-  Icon ERLANG_CONSOLE = new LayeredIcon(IconLoader.getIcon("/icons/console.png"), ERLANG_MARK);
+  Icon CONSOLE = IconLoader.getIcon("/icons/console.png");
+  Icon ERLANG_CONSOLE = new LayeredIcon(CONSOLE, ERLANG_MARK);
 
   Icon DEBUGGER_PRIMITIVE_VALUE = AllIcons.Debugger.Db_primitive;
   Icon DEBUGGER_VALUE = AllIcons.Debugger.Value;
