5420bcafd064cf312bc774b360ea92236916e545
Olya Telezhnaya <olyatelezhnaya@gmail.com>
2015-07-30 16:07:26 +0300

    Rewrite CreateFunctionQuickFix

--- testData/quickfixes/introduce_function/functionCall-after.erl
+++ testData/quickfixes/introduce_function/functionCall-after.erl
@@ -1,7 +1,7 @@
 foo() ->
   lll(1, aaaa, OOO, {}, [], init2(111)).
 
-lll(N, Aaaa, OOO, Tuple, List, Init2) ->
-  <caret>error(not_implemented).
-
 init2(N) -> N.
+
+lll(N, Aaaa, OOO, Tuple, List, Init2) ->
+  erlang:error(not_implemented).
