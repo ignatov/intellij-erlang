d739651a109ea9a3679602892335505c0a407624
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2015-08-06 12:30:53 +0300

    don't touch gen files

--- gen/org/intellij/erlang/psi/impl/ErlangCaseExpressionImpl.java
+++ gen/org/intellij/erlang/psi/impl/ErlangCaseExpressionImpl.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2012-2015 Sergey Ignatov
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 // This is a generated file. Not intended for manual editing.
 package org.intellij.erlang.psi.impl;
 
@@ -69,7 +53,7 @@ public class ErlangCaseExpressionImpl extends ErlangExpressionImpl implements Er
     return findChildByType(ERL_OF);
   }
 
-  public boolean processDeclarations(@NotNull PsiScopeProcessor processor, @NotNull ResolveState state, PsiElement lastParent, @NotNull PsiElement place) {
+  public boolean processDeclarations(PsiScopeProcessor processor, ResolveState state, PsiElement lastParent, PsiElement place) {
     return ErlangPsiImplUtil.processDeclarations(this, processor, state, lastParent, place);
   }
 
