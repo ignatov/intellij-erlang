8f682ad5647856d3b6006eb12031700aa8de1e09
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2017-11-27 17:16:02 +0300

    fix tests for small IDEs
    (cherry picked from commit 41712d2675b506da6d6c725bc6192a4dd6006262)

--- tests/org/intellij/erlang/highlighting/Erlang17SmallIdeHighlightingTest.java
+++ tests/org/intellij/erlang/highlighting/Erlang17SmallIdeHighlightingTest.java
@@ -16,12 +16,15 @@
 
 package org.intellij.erlang.highlighting;
 
+import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.testFramework.LightProjectDescriptor;
 import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor;
 import org.intellij.erlang.sdk.ErlangSdkForSmallIdes;
 import org.intellij.erlang.sdk.ErlangSdkRelease;
 import org.intellij.erlang.sdk.ErlangSdkType;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
 
 public class Erlang17SmallIdeHighlightingTest extends ErlangHighlightingTestBase {
   private static final String SDK_HOME = "testData/mockSdk-R17";
@@ -33,7 +36,12 @@ public class Erlang17SmallIdeHighlightingTest extends ErlangHighlightingTestBase
   @Nullable
   @Override
   protected LightProjectDescriptor getProjectDescriptor() {
-    return new DefaultLightProjectDescriptor();
+    return new DefaultLightProjectDescriptor() {
+      @Override
+      public Sdk getSdk() {
+        return ErlangSdkType.createMockSdk(SDK_HOME, ErlangSdkRelease.V_17_0);
+      }
+    };
   }
 
   @Override
@@ -44,8 +52,14 @@ public class Erlang17SmallIdeHighlightingTest extends ErlangHighlightingTestBase
     ErlangSdkForSmallIdes.setUpOrUpdateSdk(getProject(), SDK_HOME);
   }
 
+  @Override
+  protected void tearDown() throws Exception {
+    ErlangSdkForSmallIdes.setUpOrUpdateSdk(getProject(), "");
+    super.tearDown();
+  }
+
   public void testCorrectSdkReleaseIsReported() {
-    assertEquals(ErlangSdkRelease.V_17_0, ErlangSdkType.getRelease(getProject()));
+    assertEquals(ErlangSdkRelease.V_17_0.getOtpRelease(), Objects.requireNonNull(ErlangSdkType.getRelease(getProject())).getOtpRelease());
   }
 
   public void testErlang17SyntaxNoError() {
