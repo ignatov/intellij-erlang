0dfb40ea7a36b0a8e6987993a2f5ccc6d99a78c3
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-09-16 21:51:57 +0300

    simplify visible erlang sdk name

--- src/org/intellij/erlang/sdk/ErlangSdkRelease.java
+++ src/org/intellij/erlang/sdk/ErlangSdkRelease.java
@@ -61,7 +61,7 @@ public final class ErlangSdkRelease {
 
   @Override
   public String toString() {
-    return "Erlang/OTP " + getOtpRelease() + " [erts-" + getErtsVersion() + "]";
+    return "OTP " + getOtpRelease() + ", erts-" + getErtsVersion();
   }
 
   @Nullable
