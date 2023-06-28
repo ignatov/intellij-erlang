3fa532c406a8601c0e53237da0d0432f8f1e327a
Sergei Elin <s.elin@xpay.by>
2023-06-28 11:00:05 +0300

    Add new Erlang decimal format

--- src/org/intellij/erlang/parser/gen_lexer.sh
+++ src/org/intellij/erlang/parser/gen_lexer.sh
@@ -1,1 +1,8 @@
-java -jar ~/src/intellij-community/tools/lexer/jflex-1.7.0-SNAPSHOT.jar --skel ~/src/intellij-community/tools/lexer/idea-flex.skeleton --nobak Erlang.flex -d ../../../../../gen/org/intellij/erlang/parser/
+#!/usr/bin/env bash
+
+JFLEX_JAR=${1:-~/src/intellij-community/tools/lexer/jflex-1.7.0-SNAPSHOT.jar}
+JFLEX_SKEL=${2:-~/src/intellij-community/tools/lexer/idea-flex.skeleton}
+JFLEX_OUT=${3:-../../../../../gen/org/intellij/erlang/parser/}
+JFLEX_GRAMMAR=${4:-Erlang.flex}
+
+java -jar "$JFLEX_JAR" --skel "$JFLEX_SKEL" --nobak "$JFLEX_GRAMMAR" -d "$JFLEX_OUT"
